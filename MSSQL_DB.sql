547f8c74112b8fb2c7a5db822d000b4e037e865b
7007singh <shw12021982@gmail.com>
2023-03-02 21:49:04 +0530

    [Shweta]Add: Ability to create a employee payroll table in the payroll service database to manage employee payrolls

--- MSSQL_DB.sql
+++ MSSQL_DB.sql
@@ -3,3 +3,15 @@
 
 create database PayRoll
 use PayRoll
+
+---------UC2-EmployeePayRollTable--------
+
+CREATE TABLE EmployeePayRoll
+(
+   Id int identity(1,1) Primary key,
+   Name varchar(200),
+   Salary float,
+   StartDate date
+)
+
+
