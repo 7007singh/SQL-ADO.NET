97814b1eb446ecf3edd2e6f5067d0e768a441778
7007singh <shw12021982@gmail.com>
2023-03-07 10:01:30 +0530

    [Shweta]Add: Ability to add Gender to Employee Payroll Table and Update the Rows to reflect the correct Employee Gender

--- MSSQL_DB.sql
+++ MSSQL_DB.sql
@@ -17,10 +17,9 @@ CREATE TABLE EmployeePayRoll
 --------UC3-EmployeePayRollData---------
 
 insert into EmployeePayRoll values ('Dhoni', 45623, '2019-02-03')
-insert into EmployeePayRoll values ('Ram', 45623, '2020-02-03');
-insert into EmployeePayRoll values ('Radha', 45623, '2022-02-03');
+insert into EmployeePayRoll values ('Ram', 45623, '2020-02-03')
+insert into EmployeePayRoll values ('Radha', 45623, '2022-02-03')
 insert into EmployeePayRoll (Salary, Name, StartDate) values (465789, 'Kumar', getdate());
-insert into EmployeePayRoll (Salary, Name, StartDate) values (465789, 'Shyama', getdate());
 insert into EmployeePayroll (Salary, Name, startDate) values (12346, 'Shyama', GETDATE());
 
 ------------UC4-SelectQuerie------------
@@ -33,5 +32,10 @@ select * from EmployeePayRoll where Name = 'Radha'
 select * from EmployeePayRoll where StartDate between CAST('2023-02-03' as date) AND GETDATE();
 select * from EmployeePayRoll where StartDate between CAST('2020-02-06' as date) AND GETDATE();
 
+----------UC6-Alter&UpdateQueris--------
+
+ALTER TABLE EmployeePayRoll ADD Gender char(1);
+UPDATE EmployeePayRoll SET Gender = 'M' where Name = 'Dhoni' or Name = 'Kumar'
+UPDATE EmployeePayRoll SET Gender = 'F' where Name = 'Shyama'
 
 
