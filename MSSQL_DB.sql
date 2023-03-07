aa3274af10cc95e19c95947ecd35e9cd8e9a6557
7007singh <shw12021982@gmail.com>
2023-03-07 07:42:31 +0530

    [Shweta]Add: Ability to retrieve all the employee payroll data that is added to payroll service database

--- MSSQL_DB.sql
+++ MSSQL_DB.sql
@@ -17,10 +17,14 @@ CREATE TABLE EmployeePayRoll
 --------UC3-EmployeePayRollData---------
 
 insert into EmployeePayRoll values ('Dhoni', 45623, '2019-02-03')
+insert into EmployeePayRoll values ('Ram', 45623, '2020-02-03');
+insert into EmployeePayRoll values ('Radha', 45623, '2022-02-03');
 insert into EmployeePayRoll (Salary, Name, StartDate) values (465789, 'Kumar', getdate());
+insert into EmployeePayRoll (Salary, Name, StartDate) values (465789, 'Shyama', getdate());
+insert into EmployeePayroll (Salary, Name, startDate) values (12346, 'Shyama', GETDATE());
 
 ------------UC4-SelectQuerie------------
 
-select * from EmployeePayRoll where Name = 'Sachin';
-select * from EmployeePayRoll where StartDate between cast('2015-01-01' as date) and getdate();
+select * from EmployeePayRoll where Name = 'Radha';
+
 
